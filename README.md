59af277351e799b8157a97b06f902f99d5948201
NotEnder <endermc293@gmail.com>
2022-09-21 14:15:01 +0700

    skid web bili

---
+++ README.md
@@ -0,0 +1,8 @@
+# coded by rf0x1d | fork and re-write by tomm
+skidded by NotEnder lmao
+
+Contact me via:
+Discord: NotEnder#9204
+
+# plz
+Plz give me money or a VPS to make Minecraft HvH server pls plsplspffpogjk =((
---
+++ assets/js/text.js
@@ -0,0 +1,55 @@
+$(function () {
+  var title = document.title,
+    animSeq = ["|"],
+    animIndex = 0,
+    titleIndex = 0;
+  function doInverseSpinZeroPitch() {
+    var loadTitle = title.substring(0, titleIndex);
+    if (titleIndex > title.length) {
+      animIndex = 0;
+      titleIndex = 0;
+    }
+    if (animIndex > 1) {
+      titleIndex++;
+      animIndex = 0;
+    }
+    document.title = loadTitle + animSeq[0];
+    animIndex++;
+  }
+  window.setInterval(doInverseSpinZeroPitch, 150);
+});
+$(function () {
+  $(".typed").typed({
+    strings: [
+      "Hii~ my name is NotEnder :D",
+      "My love for you is like sodium in water... lmao",
+      "I love matcha and ice cream",
+      "I'm learning Java =)",
+      "You can call me Ender",
+      "I like to play Minecraft",
+      "Coding, coding, bug, coding, ...",
+      "There're two reasons to wake up in the morning: alarm clock and you",
+      "NotEnder#9204 is my Discord",
+      "I want money to make a MC server :sad:",
+      "iPhone, iMac, i love u",
+      "MSG",
+      "I sKiDdEd this website",
+      "HeroMC is a trash server",
+      "Nothing bypass till it got bypass - Hack client dev said",
+      "I don't know what to write",
+      "ah yess very good Anti-Cheat",
+      "Hewwo world!",
+      "Give me money to develop NovalMC :ded:",
+      "Nice, you are still reading this shiet 😋",
+      "I'll try to be good at Math to calculate the distance from here to your heart =(",
+      "2 9 3 2 0 1 0",
+      "out of idea :idk:",
+      "1 + 1 = 3",
+      "haiyaaa",
+      "fuiyoo!",
+    ],
+    typeSpeed: 135,
+    loop: true,
+    cursorChar: "|",
+  });
+});
